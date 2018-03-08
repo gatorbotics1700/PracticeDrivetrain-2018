1bf63581adf4cd60416d76bb879695f77fda2cce
Cantevisa <18vwoo@castilleja.org>
2018-03-07 21:08:10 -0800

    [STILL UNTESTED] Forgot to save one of the files... oops.

--- src/org/usfirst/frc/team1700/robot/commands/AutoCGs/AutoForward.java
+++ src/org/usfirst/frc/team1700/robot/commands/AutoCGs/AutoForward.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1700.robot.commands.AutoCGs;
 
 import org.usfirst.frc.team1700.robot.Robot;
 import org.usfirst.frc.team1700.robot.commands.Drivetrain.DriveToDistanceCommand;
+import org.usfirst.frc.team1700.robot.commands.Drivetrain.SimpleDriveForwardCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -28,6 +29,6 @@ public class AutoForward extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
-    	addSequential(new DriveToDistanceCommand(Robot.driveSubsystem.distToAutoLine, Robot.driveSubsystem.distToAutoLine));
+    	addSequential(new SimpleDriveForwardCommand(Robot.driveSubsystem.distToAutoLine));
     }
 }
