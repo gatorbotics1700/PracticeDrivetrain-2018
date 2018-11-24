23b40d2499be7ceb38830b95130a6bb744f7a614
elizag <21egoler@castilleja.org>
2018-11-24 13:52:39 -0800

    Added packages to build.gradle for testing

---
+++ src/test/java/org/usfirst/frc/team1700/robot/teleopTest/teleopTest1.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team1700.robot.teleopTest;
+
+import org.junit.jupiter.api.Test;
+import org.usfirst.frc.team1700.robot.Robot;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class teleopTest1 {
+  //  Robot.robotInit();
+
+    @Test
+    public void testFun(){
+        System.out.println("This works");
+        
+    }
+}
